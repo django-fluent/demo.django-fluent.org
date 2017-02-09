0c5e80ec0f58f48e964a943fcede1ac34bfe5d9e
Diederik van der Boor <vdboor@edoburu.nl>
2017-02-09 21:00:09 +0100

    Improve blog comments layout

---
+++ src/apps/blog/forms.py
@@ -0,0 +1,13 @@
+from fluent_comments.forms import CompactLabelsCommentForm
+
+
+class CommentForm(CompactLabelsCommentForm):
+    """
+    The comment form to use
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+
+        # Fix translations of the labels
+        self.fields['url'].label = "Website"
