1751ac269ce208847f2e0bc3a28eb6e5603c85ea
Diederik van der Boor <vdboor@edoburu.nl>
2017-12-14 18:02:00 +0100

    add helm chart for deployment

---
+++ deployment/deploy-prd.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if (( $# < 1 )); then
+  echo "$0 image-tag [helm-args]" >&2
+  exit 1
+fi
+
+CHART="./chart/"
+RELEASE_NAME="demo-django-fluent-org"
+TAG="${1:-latest}"; shift
+
+cd `dirname $0`
+helm upgrade --install --reset-values "$RELEASE_NAME" "$CHART" -f "values-prd.yml" --set="imageTag=$TAG" "$@"
