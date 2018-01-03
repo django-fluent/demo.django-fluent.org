b15f201dfef2188931e279f3048dea66893effea
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-03 15:53:06 +0100

    full support redis for caching
    This is a much better default then memcached

--- src/fluentdemo/settings/defaults.py
+++ src/fluentdemo/settings/defaults.py
@@ -217,7 +217,7 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 CACHES = {
-    'default': env.cache(default='memcache://127.0.0.1:11211?TIMEOUT=86400&KEY_PREFIX=fluentdemo'),
+    'default': env.cache(default='redis://127.0.0.1:11211?TIMEOUT=86400&KEY_PREFIX=fluentdemo'),
 }
 
 DATABASES = {
