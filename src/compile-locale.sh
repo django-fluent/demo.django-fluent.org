8661eafc95260fbd053c5854c6370fd062438f6b
Diederik van der Boor <vdboor@edoburu.nl>
2018-01-30 15:49:24 +0100

    modernize frontend
    - use gulp folder
    - use {% static %} tag
    - remove IE8 css
    - copy vendor files from node_modules
    - bump vendor files, e.g. jQuery 3.x

--- src/compile-locale.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-unset DJANGO_SETTINGS_MODULE
-
-django-admin.py compilemessages
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -12,56 +12,11 @@
  */
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var livereload = require('gulp-livereload');
-var del = require('del');
+var gulp = require('gulp'),
+  requireDir = require('require-dir');
 
-var paths = {
-  sass: './frontend/sass/',
-  css: './frontend/static/frontend/css/',
-};
+// Import all tasks
+requireDir('./frontend/gulp/tasks', {recurse: true});
 
-paths.sass_glob = paths.sass + '**/*.scss';
-
-var sass_options = {
-  // See https://github.com/sass/node-sass
-  outputStyle: 'expanded',
-  includePaths: [
-    './frontend/sass-vendor/',
-    './node_modules/bootstrap-sass/assets/stylesheets/',
-  ],
-  precision: 5,
-};
-
-var livereload_options = {
-  host: '127.0.0.1',
-};
-
-gulp.task('sass', function () {
-  // Task to compile sass
-  gulp.src(paths.sass_glob)
-    .pipe(sourcemaps.init())
-    .pipe(sass(sass_options).on('error', sass.logError))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.css))
-    .pipe(livereload());
-});
-
-gulp.task('sass:watch', ['sass'], function () {
-  // Watch and recompile on changes
-  livereload.listen(livereload_options);
-  gulp.watch(paths.sass_glob, ['sass'])
-    .on('change', function(evt) {
-        console.log('[watcher] File ' + evt.path.replace(/.*(?=sass)/,'') + ' was ' + evt.type + ', compiling...');
-    });
-});
-
-gulp.task('clean', function (cb) {
-  // clean task
-  del([paths.css + '*.css', '!' + paths.css + 'user-styles.css'], cb);
-});
-
-// Default actions
-gulp.task('default', ['sass:watch']);
+// Define default task
+gulp.task('default', ['clean', 'vendor', 'sass']);
