3960f4382067cdc1988b88ed26613ba9c5674590
Diederik van der Boor <vdboor@edoburu.nl>
2022-04-12 12:38:52 +0200

    Bump packages, Django 2.2.28 security advisory

--- src/fluentdemo/urls.py
+++ src/fluentdemo/urls.py
@@ -46,8 +46,8 @@ urlpatterns = [
     # e.g. /nl/... or /en/....
 
     # Django admin
-    url(r'^admin/', admin.site.urls),
     url(r'^admin/filebrowser/', fb_site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^admin/util/tags/', include(taggit_selectize.urls)),
     url(r'^admin/util/tinymce/', include(tinymce.urls)),
     url(r'^admin/util/tools/', include(admin_tools.urls)),
